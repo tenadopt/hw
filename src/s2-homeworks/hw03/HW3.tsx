986725824f544c38834778d2bbecd2971e605964
Tenadopt <yury.ignatiev@gmail.com>
2023-07-18 17:30:18 +0300

    hw3 done

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React, {Dispatch, useState} from 'react'
 import { v1 } from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
 /*
-* 1 - описать тип UserType
-* 2 - указать нужный тип в useState с users
-* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами
-* 4 - в файле GreetingContainer.tsx дописать типизацию пропсов
-* 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error
-* 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback
+* 1 - описать тип UserType done
+* 2 - указать нужный тип в useState с users done
+* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами done
+* 4 - в файле GreetingContainer.tsx дописать типизацию пропсов done
+* 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error done
+* 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback done
 * 7 - в файле GreetingContainer.tsx дописать логику функций pureAddUser, pureOnBlur, pureOnEnter и проверить их тестами
 * 8 - в файле GreetingContainer.tsx вычислить количество добавленных и имя последнего (totalUsers, lastUserName)
 * 9 - в файле Greeting.tsx дописать типизацию пропсов
@@ -19,20 +19,19 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
-    }
+export const pureAddUserCallback = (name: string, setUsers: Dispatch<UserType[]>, users: UserType[]) => { // need to fix any
+    const user = { _id: v1(), name: name}
     setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 
